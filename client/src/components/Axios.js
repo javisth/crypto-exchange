1bc58d2e0ba74a87a3717287a173c8ca7193ebca
javisth <javisth@gmail.com>
2021-11-08 12:07:19 -0500

    Update Axios.js

--- client/src/components/Axios.js
+++ client/src/components/Axios.js
@@ -1,55 +1,62 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import NumberFormat from 'react-number-format';
-import Recommendation from './Recommendation';
-
-
+import React, { Component } from "react";
+import axios from "axios";
+import NumberFormat from "react-number-format";
+import Recommendation from "./Recommendation";
 
 class Axios extends Component {
-    constructor(props) {
-      super(props);
-  
+  constructor(props) {
+    super(props);
+
     // holds response from API
-      this.state = {
-        cryptos: []
-      };
-    }
+    this.state = {
+      cryptos: [],
+    };
+  }
 
-    componentDidMount() {
-      const fsymsList = 'BTC,ETH'
-      axios
-        .get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + fsymsList + '&tsyms=USD,EUR')
-    .then(res => {
-      const cryptos = res.data;
-      console.log(cryptos);
-      this.setState({cryptos});
-    });
-    }
+  componentDidMount() {
+    const fsymsList = "BTC,ETH";
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" +
+          fsymsList +
+          "&tsyms=USD,EUR"
+      )
+      .then((res) => {
+        const cryptos = res.data;
+        console.log(cryptos);
+        this.setState({ cryptos });
+        this.props.setOtherExchangeValue("otherbtcusd", cryptos["BTC"].USD);
+        this.props.setOtherExchangeValue("otherethusd", cryptos["ETH"].USD);
+      });
+  }
 
-    render() {
-      return (
-        <div className="card mr-0 custom-card">
-          <div className="card-body">
-                  {Object.keys(this.state.cryptos).map((key) => (
-                      <div key={key}>
-                        {key}
-                        <br/>
-                      	<NumberFormat
-				value={parseInt(this.state.cryptos[key].USD)}
-				displayType={'text'}
-				decimalprecision={2} 
-				thousandSeparator={true} 
-				prefix={' $'} />
-                            <Recommendation val2={this.state.cryptos[key]}/>
-                            {console.log(typeof (this.state.cryptos[key]+''))}
-                            <br/>
-                        </div>
-                  ))}
-          </div>
+  render() {
+    return (
+      <div className="card mr-0 custom-card">
+        <div className="card-body">
+          {Object.keys(this.state.cryptos).map((key) => (
+            <div key={key}>
+              {key}
+              <br />
+              <NumberFormat
+                value={parseInt(this.state.cryptos[key].USD)}
+                displayType={"text"}
+                decimalprecision={2}
+                thousandSeparator={true}
+                prefix={" $"}
+              />
+              <Recommendation
+                value1={this.state.cryptos[key].USD}
+                value2={this.props.btceth[key.toLowerCase()]}
+              />
+              {console.log(typeof (this.state.cryptos[key] + ""))}
+              <br />
+            </div>
+          ))}
         </div>
-      );
-      
-    }
-  } 
-  
-  export default Axios;
+      </div>
+    );
+  }
+}
+
+export default Axios;
