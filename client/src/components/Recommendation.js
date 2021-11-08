93b4683129ed157889a4a788950e4316921fb1dc
javisth <javisth@gmail.com>
2021-11-08 12:07:42 -0500

    Update Recommendation.js

--- client/src/components/Recommendation.js
+++ client/src/components/Recommendation.js
@@ -1,23 +1,17 @@
-import React from 'react';
-let value=0;
-let val2 = 0;
+import React from "react";
+
 const Recommendation = (props) => {
-    if(typeof props.value == "string"){
-        val2 = parseInt(props.value);
-    }
-    else{
-        value = typeof parseInt(props.value) === 'number' && !isNaN(parseInt(props.value)) ? Math.round(parseInt(props.value)) : props.value;
-    }
-    return (
-        <div className="card mr-0 custom-card">
-            <div className="card-body">
-                Recommendation:<br/>
-                {console.log(val2)}
-                {console.log(value)}
-                {val2<value?'Buy':'Sell'}
-            </div>  
-        </div>
-    )
+  return (
+    <div className="card mr-0 custom-card">
+      <div className="card-body">
+        Recommendation:
+        <br />
+        {console.log("---value1: ", props.value1)}
+        {console.log("---value2: ", props.value2)}
+        {props.value1 < props.value2 ? "Buy" : "Sell"}
+      </div>
+    </div>
+  );
 };
 
-  export default Recommendation;
+export default Recommendation;
